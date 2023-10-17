fdf9fee9579124bf2ce86bf954edff618e8ffed1
vignesh2336 <147582733+vignesh2336@users.noreply.github.com>
2023-10-17 10:30:43 +0530

    #include <stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a<b)     {         printf("Profit");     }     else if(a>b)     {         printf("Loss");     }      else if(a==b)      {          printf("No Profit and No Loss");      } }

---
+++ Profit_or_Loss.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a<b)
+    {
+        printf("Profit");
+    }
+    else if(a>b)
+    {
+        printf("Loss");
+    }
+     else if(a==b)
+     {
+         printf("No Profit and No Loss");
+     }
+}
